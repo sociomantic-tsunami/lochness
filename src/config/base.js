04c282716d0cddf5d178ead21e6cba38d5d9c8fb
Mahmoud Zakria <mahmoud.zakria@sociomantic.com>
2018-09-07 16:51:47 +0200

    updated copyright statments

--- src/config/base.js
+++ src/config/base.js
@@ -1,13 +1,11 @@
 /*
- * Copyright (c) 2017 dunnhumby Germany GmbH.
+ * Copyright (c) 2017-2018 dunnhumby Germany GmbH.
  * All rights reserved.
  *
  * This source code is licensed under the MIT license found in the LICENSE file
  * in the root directory of this source tree.
  *
  */
-
-
 
 // Settings configured here will be merged into the final config object.
 export default {
